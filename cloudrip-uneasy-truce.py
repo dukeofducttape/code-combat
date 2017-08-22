4631f67119a3c1f5101776dc6bf6432b59e1c855
dukeofducttape <31116407+dukeofducttape@users.noreply.github.com>
2017-08-22 06:45:56 -0400

    Create cloudrip-uneasy-truce.py

---
+++ cloudrip-uneasy-truce.py
@@ -0,0 +1,22 @@
+# Summon one soldier for every ogre to the south of you!
+# Don't count the ogres to the north!
+
+# Accept an array of units as the parameter.
+# Return only the units to the south of the hero.
+def findSouthernUnits(units):
+    southernUnits = []
+    for unit in units:
+        if unit.pos.y < hero.pos.y:
+            # Add the unit to the array with: append()
+            southernUnits.append(unit)
+    return southernUnits
+
+
+while True:
+    friends = hero.findFriends()
+    enemies = hero.findEnemies()
+    # Use findSouthernUnits to get enemies to the south.
+    # If there are more ogres south of you than friends.
+    if len(findSouthernUnits(enemies)) > len(friends):
+        # Then summon another "soldier".
+        hero.summon("soldier")
