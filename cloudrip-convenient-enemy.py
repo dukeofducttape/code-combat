53082f5c265bed06c99dbb886c01c64c08842a69
dukeofducttape <31116407+dukeofducttape@users.noreply.github.com>
2017-08-22 07:25:59 -0400

    Create cloudrip-convenient-enemy.py

---
+++ cloudrip-convenient-enemy.py
@@ -0,0 +1,29 @@
+# Ogres are hiding in woods. Protect the peasants.
+# The last word in the peasants' messages are a hint.
+
+for x in range(8, 73, 16):
+    hero.moveXY(x, 22)
+    # Peasants know whom to summon.
+    peasant = hero.findNearest(hero.findFriends())
+    message = peasant.message
+    if message:
+        # Words are seaparated by whitespaces.
+        words = message.split(" ")
+        # "words" is an array of words from the "message".
+        # Get the last word. It's the required unit type.
+        unitType = words[len(words) - 1]
+        # Summon the required unit type.
+        hero.say(unitType)
+        hero.summon(unitType)
+
+for i in range(len(hero.built)):
+    unit = hero.built[i]
+    # Command the unit to defend the unit's position.
+    hero.command(unit, "defend", unit.pos)
+
+# Defend the last point yourself:
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        while enemy.health > 0:
+            hero.attack(enemy)
