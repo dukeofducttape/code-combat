02302a4a214421d54a8b93cc80d535aab3b1a02c
dukeofducttape <31116407+dukeofducttape@users.noreply.github.com>
2017-08-22 05:21:10 -0400

    Update cloudrip-hunting-party.py

--- cloudrip-hunting-party.py
+++ cloudrip-hunting-party.py
@@ -3,8 +3,6 @@
 # You can use findNearestEnemy() on your soldiers to get their nearest enemy instead of yours.
 
 
-# Use range to make an array to loop over.
-# Match the friends to the points and command them to move
 while True:
     friends = hero.findFriends()
     for j in range(len(friends)):
@@ -12,7 +10,20 @@ while True:
         enemy = friend.findNearestEnemy()
         if enemy:
             hero.command(friend, "attack", enemy)
+        elif friend.health < friend.maxHealth * 0.5:
+            newX = friend.pos.x - 5
+            newY = friend.pos.y
+            hero.command(friend, "move", {"x":newX, "y":newY})
         else:
             newX = friend.pos.x + 5
             newY = friend.pos.y
             hero.command(friend, "move", {"x":newX, "y":newY})
+
+while True:
+    heroEnemy = hero.findNearestEnemy()
+    if enemy:
+        hero.attack(enemy)
+    else:
+        heroX = hero.pos.x + 5
+        heroY = hero.pos.y
+        hero.moveXY(heroX, heroY)
