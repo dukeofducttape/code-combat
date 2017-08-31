60a7094acfb008e9f2644a5aaeefe4c4f9d5b004
dukeofducttape <31116407+dukeofducttape@users.noreply.github.com>
2017-08-31 04:36:47 -0400

    Update sarven-sarven-brawl.py

--- sarven-sarven-brawl.py
+++ sarven-sarven-brawl.py
@@ -3,21 +3,19 @@
 # If you lose, you must wait a day before you can resubmit.
 # Remember, each submission gets a new random seed.
 
-arcDefendPoints = [{"x":75, "y":77}, {"x":75, "y":70}, {"x":75, "y":63}]
-heroDefendPoint = {"x":70, "y":70}
+arcDefendPoints = [{"x":75, "y":77}, {"x":75, "y":74}, {"x":75, "y":67}, {"x":75, "y":63}]
+heroDefendPoint = {"x":73, "y":70}
 
-enemyTypes = [{"unittype": "door", "danger": 1000, "focus": 200},
-            {"unittype": "burl", "danger": 10, "focus": 20}, 
-            {"unittype": "shaman", "danger": 10, "focus": 50}, 
+enemyTypes = [{"unittype": "shaman", "danger": 10, "focus": 30}, 
             {"unittype": "warlock", "danger": 10, "focus": 30},
-            {"unittype": "witch", "danger": 8, "focus": 50}, 
-            {"unittype": "brawler", "danger": 7, "focus": 20}, 
-            {"unittype": "ogre", "danger": 5, "focus": 20}, 
-            {"unittype": "chieftain", "danger": 6, "focus": 35}, 
-            {"unittype": "thrower", "danger": 3, "focus": 24}, 
-            {"unittype": "fangrider", "danger": 4, "focus": 24},
-            {"unittype": "scout", "danger": 3, "focus": 15},
-            {"unittype": "munchkin", "danger": 2, "focus": 15},
+            {"unittype": "witch", "danger": 8, "focus": 30}, 
+            {"unittype": "brawler", "danger": 7, "focus": 30}, 
+            {"unittype": "ogre", "danger": 5, "focus": 30}, 
+            {"unittype": "chieftain", "danger": 6, "focus": 30}, 
+            {"unittype": "thrower", "danger": 3, "focus": 30}, 
+            {"unittype": "fangrider", "danger": 4, "focus": 30},
+            {"unittype": "scout", "danger": 3, "focus": 30},
+            {"unittype": "munchkin", "danger": 2, "focus": 30},
             {"unittype": "sand-yak", "danger": -1, "focus": -1},
             {"unittype": "ice-yak", "danger": -1, "focus": -1},
             {"unittype": "yak", "danger": -1, "focus": -1}]
@@ -46,9 +44,9 @@ def attack(target):
     if target:
         if hero.distanceTo(target) > 10:
             moveTo(target.pos)
-        elif hero.isReady("bash") and target.health > 21:
+        elif hero.isReady("bash") and target.health > 42:
             hero.bash(target)
-        elif hero.isReady("power-up") and target.health > 42:
+        elif hero.isReady("power-up") and target.health > 63:
             hero.powerUp()
         elif hero.isReady("attack"):
             hero.attack(target)
@@ -56,14 +54,14 @@ def attack(target):
             hero.shield()
 
 def summonTroops():
-    if hero.gold >= hero.costOf('archer'):
+    if hero.gold >= hero.costOf('archer') and hero.pos.x > 60:
         hero.summon('archer')
 
 def commandTroops():
     friends = hero.findFriends()
     for i in range(len(friends)):
         friend = friends[i]
-        arcDefendPoint = arcDefendPoints[i % 3]
+        arcDefendPoint = arcDefendPoints[i % 4]
         if friend.type == "archer":
             hero.command(friend, "defend", arcDefendPoint)
 
@@ -71,7 +69,7 @@ while True:
     summonTroops()
     commandTroops()
     target = findTarget()
-    if target:
+    if target and target.pos.x >= 40:
         attack(target)
     else:
         moveTo(heroDefendPoint)
